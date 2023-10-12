b575f16d6c41dbf89f77caf1b9df166ae7f9aa1e
Hossam Othman <rowheadrex@gmail.com>
2023-10-12 16:39:24 +0200

    Node Events

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const {format} = require('date-fns');
-const { v4: uuid } = require('uuid')
+const logEvents = require('./logEvents');
 
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+const EventEmitter = require('events');
 
+// basically we're extending a class... 
+class MyEmitter extends EventEmitter {};
 
-console.log(uuid())
+// initialize object that we'Re going to create...
+const myEmitter = new MyEmitter();
+
+// add a listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    // emit event... just to add a little bit of delay to it
+    myEmitter.emit('log', '3rd log event emitted...!');
+}, 2000);
